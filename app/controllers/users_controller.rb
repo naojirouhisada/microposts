bc8a314a997cb57a072806710a78d7e98ec1bb28
naojirouhisada <hn1213sx00vytr@yahoo.co.jp>
2015-08-13 06:20:54 +0000

    added Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
   def show #追加
     @user = User.find(params[:id])
@@ -10,20 +11,36 @@ class UsersController < ApplicationController
     
   def create
     @user = User.new(user_params)
-    if @user.save      
+    if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user #ここを修正
+      redirect_to @user
     else
       render 'new'
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
   
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'Profileを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
     
   private
     
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name,:area, :email, :password,
                                    :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
