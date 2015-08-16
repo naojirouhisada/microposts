27b9fbe8fa26de11d6e88e4b430a98696079e936
naojirouhisada <hn1213sx00vytr@yahoo.co.jp>
2015-08-16 06:53:56 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,23 +8,31 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :microposts
     
-    has_many :following_relationships, class_name: "Relationship",
-                                       foreign_key: "followe_id",
-                                       dependent:   :destroy
+   
+    has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
     has_many :following_users, through: :following_relationships, source: :followed
     
-    #他のユーザーをフォローする
+    has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+    has_many :follower_users, through: :follower_relationships, source: :follower
+    
+    # 他のユーザーをフォローする
     def follow(other_user)
         following_relationships.create(followed_id: other_user.id)
     end
-    
-     # フォローしているユーザーをアンフォローする
-     def unfollow(other_user)
-         following_relationships.find.by(followed_id: other_user.id).destroy
-     end
-     
-     # あるユーザーをフォローしているかどうか？
-     def following?(other_user)
-         following_users.include?(other_user)
-     end
+
+    # フォローしているユーザーをアンフォローする
+    def unfollow(other_user)
+        following_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    # あるユーザーをフォローしているかどうか？
+    def following?(other_user)
+        following_users.include?(other_user)
+    end
 end
+
+ 
