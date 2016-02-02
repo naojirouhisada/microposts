709f6a09fee1e3f9b728acfb5525b9951bfee5da
naojirouhisada <hn1213sx00vytr@yahoo.co.jp>
2016-02-02 14:41:17 +0000

    edit microposts-heroku

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,10 +1,14 @@
+
 class MicropostsController < ApplicationController
      before_action :logged_in_user, only: [:create]
+
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
     @user = @micropost.user
+
     if @micropost.save
+      
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :check_logined, only: :show
   def new
   end
   
